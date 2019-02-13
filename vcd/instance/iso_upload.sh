7a563949e5314b595ca751bfda01b45f2d2e8be6
mdacrema <matteo.dacrema@gmail.com>
2019-02-13 11:19:57 +0000

    fixed sh script for iso and ova upload

--- vcd/instance/iso_upload.sh
+++ vcd/instance/iso_upload.sh
@@ -2,11 +2,11 @@
 
 set -e
 OUTPUT=$(ovftool -st="ISO" $ISO_PATH "vcloud://$VCD_USERNAME:$VCD_PASSWORD@$VCD_CLOUD_URL?org=$VCD_ORG&vdc=$VCD_VDC&media=$ISO_NAME&catalog=$VCD_CATALOG")|| true
-#to check
-if [[ $OUTPUT == *"vApp name already found"* ]]; then
-	echo ${TEMPLATE_NAME} already exist, skip image upload
+echo $OUTPUT > iso_out.txt
+if [[ $OUTPUT == *"Media name already found"* ]]; then
+	echo ${ISO_NAME} already exist, skip image upload
 	exit 0
 elif [[ $OUTPUT == *"Transfer Completed"* ]]; then
-	jq -r -n --arg template_name "${TEMPLATE_NAME}" --arg output  "${TEMPLATE_NAME} uploaded successfully" '{"template_name":$template_name,"output":$output}'
+	jq -r -n --arg template_name "${ISO_NAME}" --arg output  "${ISO_NAME} uploaded successfully" '{"template_name":$template_name,"output":$output}'
 fi
 #jq -r -n --arg template_name "Ubuntu-16.04-CloudInit" --arg output  "Ubuntu-16.04-CloudInit uploaded successfully" '{"template_name":$template_name,"output":$output}'
