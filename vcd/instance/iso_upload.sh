5945dfb81d95939f8691cf2d570c13e3abb1db68
mdacrema <matteo.dacrema@gmail.com>
2019-02-13 10:36:47 +0000

    restored ovftool upload

--- vcd/instance/iso_upload.sh
+++ vcd/instance/iso_upload.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#set -e
-#OUTPUT=$(ovftool -st="ISO" $ISO_PATH "vcloud://$VCD_USERNAME:$VCD_PASSWORD@$VCD_CLOUD_URL?org=$VCD_ORG&vdc=$VCD_VDC&media=$ISO_NAME&catalog=$VCD_CATALOG")|| true
-##to check
-#if [[ $OUTPUT == *"vApp name already found"* ]]; then
-#	echo ${TEMPLATE_NAME} already exist, skip image upload
-#	exit 0
-#elif [[ $OUTPUT == *"Transfer Completed"* ]]; then
-#	jq -r -n --arg template_name "${TEMPLATE_NAME}" --arg output  "${TEMPLATE_NAME} uploaded successfully" '{"template_name":$template_name,"output":$output}'
-#fi
-jq -r -n --arg template_name "Ubuntu-16.04-CloudInit" --arg output  "Ubuntu-16.04-CloudInit uploaded successfully" '{"template_name":$template_name,"output":$output}'
+set -e
+OUTPUT=$(ovftool -st="ISO" $ISO_PATH "vcloud://$VCD_USERNAME:$VCD_PASSWORD@$VCD_CLOUD_URL?org=$VCD_ORG&vdc=$VCD_VDC&media=$ISO_NAME&catalog=$VCD_CATALOG")|| true
+#to check
+if [[ $OUTPUT == *"vApp name already found"* ]]; then
+	echo ${TEMPLATE_NAME} already exist, skip image upload
+	exit 0
+elif [[ $OUTPUT == *"Transfer Completed"* ]]; then
+	jq -r -n --arg template_name "${TEMPLATE_NAME}" --arg output  "${TEMPLATE_NAME} uploaded successfully" '{"template_name":$template_name,"output":$output}'
+fi
+#jq -r -n --arg template_name "Ubuntu-16.04-CloudInit" --arg output  "Ubuntu-16.04-CloudInit uploaded successfully" '{"template_name":$template_name,"output":$output}'
