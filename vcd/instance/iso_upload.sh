9781c555801763ffc78addbcbc4647ed17079656
mdacrema <matteo.dacrema@gmail.com>
2019-02-14 09:11:21 +0000

    add vm power on after cd-attach

--- vcd/instance/iso_upload.sh
+++ vcd/instance/iso_upload.sh
@@ -10,6 +10,7 @@ echo $VCD_URL > iso_url.txt
 if [[ $OUTPUT == *"Media name already found"* ]]; then
 	jq -r -n --arg template_name "${ISO_NAME}" --arg output  "${ISO_NAME} already exist" '{"template_name":$template_name,"output":$output}'
 elif [[ $OUTPUT == *"Transfer Completed"* ]]; then
+	sleep 60
 	jq -r -n --arg template_name "${ISO_NAME}" --arg output  "${ISO_NAME} uploaded successfully" '{"template_name":$template_name,"output":$output}'
 fi
 #jq -r -n --arg template_name "Ubuntu-16.04-CloudInit" --arg output  "Ubuntu-16.04-CloudInit uploaded successfully" '{"template_name":$template_name,"output":$output}'
--- vcd/instance/power_on.sh
+++ vcd/instance/power_on.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 TOKEN=$(curl -I -s -i -k -H "Accept:application/*+xml;version=1.5" -u $VCD_AUTH -X POST $VCD_URL/sessions |grep  auth |awk {'print $2'} |sed -s 's/\r$//')
+echo $TOKEN > vcd_token.txt
 VM_URL=$(curl -s -i -k -H "Accept:application/*+xml;version=1.5" -H "x-vcloud-authorization: $TOKEN" -X GET  "$VCD_URL/query?type=vm" |grep name=\"$VM_NAME\"  |grep -o "href.*" | sed 's/\s.*$//' |sed -e 's/href="//' |sed -s 's/"//')
+echo $VM_URL > vcd_poweron_url.txt
 POWERON=$(curl -s -i -k -H "Accept:application/*+xml;version=1.5" -H "x-vcloud-authorization: $TOKEN" -X POST $VM_URL"/power/action/powerOn")
+echo $POWERON > vcd_poweron.txt
 jq -r -n --arg vm_name "${VM_NAME}" --arg output  "${VM_NAME} powered on" '{"vm_name":$vm_name,"output":$output}'
